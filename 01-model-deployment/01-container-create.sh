2204345f5fd4b3c625cf93eceac190aa64398a86
root <root@hostname-kczbs.foreman.pxe>
2026-09-20 07:06:58 +0000

    Add container creation script

---
+++ 01-model-deployment/01-container-create.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+docker run -it \
+  --name zjw-vllm-qwen \
+  --net=host \
+  --privileged \
+  --shm-size=1g \
+  --device=/dev/davinci0 \
+  --device=/dev/davinci1 \
+  --device=/dev/davinci2 \
+  --device=/dev/davinci3 \
+  --device=/dev/davinci4 \
+  --device=/dev/davinci5 \
+  --device=/dev/davinci6 \
+  --device=/dev/davinci7 \
+  --device=/dev/davinci8 \
+  --device=/dev/davinci9 \
+  --device=/dev/davinci10 \
+  --device=/dev/davinci11 \
+  --device=/dev/davinci12 \
+  --device=/dev/davinci13 \
+  --device=/dev/davinci14 \
+  --device=/dev/davinci15 \
+  --device=/dev/davinci_manager \
+  --device=/dev/devmm_svm \
+  --device=/dev/hisi_hdc \
+  -v /usr/local/dcmi:/usr/local/dcmi \
+  -v /usr/local/Ascend/driver/tools/hccn_tool:/usr/local/Ascend/driver/tools/hccn_tool \
+  -v /usr/local/bin/npu-smi:/usr/local/bin/npu-smi \
+  -v /usr/local/Ascend/driver/lib64:/usr/local/Ascend/driver/lib64 \
+  -v /usr/local/Ascend/driver/version.info:/usr/local/Ascend/driver/version.info \
+  -v /etc/ascend_install.info:/etc/ascend_install.info \
+  -v /home:/home \
+  -v /home1:/home1 \
+  quay.nju.edu.cn/ascend/vllm-ascend:v0.23.0-a3 \
+  bash
